fdb5fddab999a1a0ff34fe2a8a67d9a1123600f6
Krrish Chaudhary <chaudhariikrishna1@gmail.com>
2026-05-17 11:57:31 +0545

    fix: Database error

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 from decouple import config
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -51,11 +52,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3")
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
